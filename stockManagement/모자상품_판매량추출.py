59876234bc1ccef4289f97505dc6ced738126369
shong <shongtruth@gmail.com>
2024-10-31 11:50:46 +0900

    feat: 판매채널 추가 표시

--- stockManagement/모자상품_판매량추출.py
+++ stockManagement/모자상품_판매량추출.py
@@ -6,7 +6,7 @@ sheet1Name = '판매량 체크'
 firstCell = 2
 lastCell = wb[sheet1Name].max_row + 1
 
-# 모션 옵션리스트
+# 모자 옵션리스트
 prdCapOptionList = [
   '토밍이세트/아이보리/free',
   '토밍이세트/브라운/free',
@@ -78,12 +78,18 @@ prdCapOptionList = [
   '스마일비니/브라운/free',
 ]
 
+# 모자 판매채널 정보
+prdCapChannelList = {}
+
+for i in range(firstCell, lastCell):
+  prdCapChannelList[wb[sheet1Name].cell(i, 25).value] = wb[sheet1Name].cell(i, 26).value
+
 # 모자판매정보 생성
 capSellList = {}
 for i in range(firstCell, lastCell):
   try:
     if wb[sheet1Name].cell(i, 1).value in prdCapOptionList:
-      capSellList[wb[sheet1Name].cell(i, 1).value] = wb[sheet1Name].cell(i, 2).value
+      capSellList[wb[sheet1Name].cell(i, 1).value] = str(prdCapChannelList[wb[sheet1Name].cell(i, 1).value]) + " / " + str(wb[sheet1Name].cell(i, 2).value)
   except:
     pass
   
