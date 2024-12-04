590c036d069a453caf10a4c1c8ad6f77a28d435f
shong <shongtruth@gmail.com>
2024-12-04 11:03:36 +0900

    fix: 재고 보충 필요상품 분류 조건 보완

--- stockManagement/채널별_재고수량_트래킹/보리보리/보리보리 재고수량 트래킹.py
+++ stockManagement/채널별_재고수량_트래킹/보리보리/보리보리 재고수량 트래킹.py
@@ -124,8 +124,9 @@ for i in range(first_row, last_row):
             ws.cell(row=i, column=colNum).fill = fillData2
             
     if stockList[ws.cell(i, 22).value] != 0:
-      if int(ws.cell(row=i, column=12).value) <= 3:
-        impendingPrdList.append("○ {} / 상품코드 : {} / 실재고 : {} / 임시재고 : {} / 사용여부 : {} / 데이터파일 기준 재고 : {}".format(ws.cell(i, 22).value, ws.cell(i, 7).value, ws.cell(i, 12).value, ws.cell(i, 13).value, ws.cell(i, 16).value, stockList[ws.cell(i, 22).value]))
+      if int(ws.cell(row=i, column=12).value) <= 3 and int(ws.cell(row=i, column=12).value) >= 0:
+        if ws.cell(row=i, column=16).value == 'Y':
+          impendingPrdList.append("○ {} / 상품코드 : {} / 실재고 : {} / 임시재고 : {} / 사용여부 : {} / 데이터파일 기준 재고 : {}".format(ws.cell(i, 22).value, ws.cell(i, 7).value, ws.cell(i, 12).value, ws.cell(i, 13).value, ws.cell(i, 16).value, stockList[ws.cell(i, 22).value]))
   except:
     continue
   
