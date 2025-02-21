e9d4a93793a6233137263e4464195ff35efd934a
shong <shongtruth@gmail.com>
2025-02-21 17:44:39 +0900

    feat: 상품 호수별 판매량 추출 기능 추가

--- exportSalesData/exportSalesData.py
+++ exportSalesData/exportSalesData.py
@@ -12,12 +12,26 @@ last_row = ws.max_row + 1
 # JSON 파일로 저장
 jsonData = {}
 jsonData["data"] = []
+
+# 상품 호수별 판매정보 생성
+salesPrdSizeQnt = {}
+for i in range(first_row, last_row):
+  if ws.cell(i, 10).value not in salesPrdSizeQnt:
+    salesPrdSizeQnt[ws.cell(i, 10).value] = {'{}'.format(ws.cell(i, 11).value): ws.cell(i, 12).value}
+  else:
+    salesPrdSizeQnt[ws.cell(i, 10).value].setdefault('{}'.format(ws.cell(i, 11).value), ws.cell(i, 12).value)
+  
+print(salesPrdSizeQnt)
   
 for i in range(first_row, last_row):
-  jsonData["data"].append({
-    "prdName": ws.cell(i, 1).value.replace("토밍이세트", "토밍이모자세트").replace("해피스노우세트", "해피스노우모자세트"),
-    "salesCnt": ws.cell(i, 2).value
-  })
+  if ws.cell(row=i, column=1).value == None or ws.cell(row=i, column=1).value == '':
+    continue
+  else:
+    jsonData["data"].append({
+      "prdName": ws.cell(i, 1).value.replace("토밍이세트", "토밍이모자세트").replace("해피스노우세트", "해피스노우모자세트"),
+      "salesCnt": ws.cell(i, 2).value,
+      "sizeQnt": salesPrdSizeQnt[ws.cell(i, 1).value.replace("토밍이세트", "토밍이모자세트").replace("해피스노우세트", "해피스노우모자세트")]
+    })
   
 print(jsonData)
 
