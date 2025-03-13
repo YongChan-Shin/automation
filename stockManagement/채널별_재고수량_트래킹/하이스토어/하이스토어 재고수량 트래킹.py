87694b2593d2d78d3286beb320aa9e70bca2cf81
shong <shongtruth@gmail.com>
2025-03-13 14:29:03 +0900

    chore: 상품번호 정보 추가 추출

--- stockManagement/채널별_재고수량_트래킹/하이스토어/하이스토어 재고수량 트래킹.py
+++ stockManagement/채널별_재고수량_트래킹/하이스토어/하이스토어 재고수량 트래킹.py
@@ -157,9 +157,9 @@ for i in range(first_row, last_row):
         if int(ws.cell(row=i, column=13).value) != 0:
           print("{}/{}".format(ws.cell(i, 28).value, stockList[ws.cell(i, 28).value]))
           if ws.cell(i, 28).value in soldoutPrdCSList:
-            stockErrList.append("○ {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : 0".format(ws.cell(i, 28).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
+            stockErrList.append("○ {} / 상품번호 : {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : 0".format(ws.cell(i, 28).value, ws.cell(i, 2).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
           else:
-            stockErrAutoList.append("※ 판매량차감 자동품절 상품(CS팀에서 품절로 전달되지 않은 상품) ※\n○ {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : 0".format(ws.cell(i, 28).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
+            stockErrAutoList.append("※ 판매량차감 자동품절 상품(CS팀에서 품절로 전달되지 않은 상품) ※\n○ {} / 상품번호 : {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : 0".format(ws.cell(i, 28).value, ws.cell(i, 2).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
             
           for colNum in range(1, 30):
             ws.cell(row=i, column=colNum).fill = fillData2
@@ -168,12 +168,12 @@ for i in range(first_row, last_row):
       if ws.cell(row=i, column=6).value == "판매중":
         if int(ws.cell(row=i, column=13).value) <= 3:
           if stockList[ws.cell(i, 28).value] > int(ws.cell(row=i, column=13).value):
-            impendingPrdList.append("○ {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : {}".format(ws.cell(i, 28).value, ws.cell(i, 6).value, ws.cell(i, 13).value, stockList[ws.cell(i, 28).value]))
+            impendingPrdList.append("○ {} / 상품번호 : {} / 상태 : {} / 재고수량 : {} / 데이터파일 기준 재고 : {}".format(ws.cell(i, 28).value, ws.cell(i, 2).value, ws.cell(i, 6).value, ws.cell(i, 13).value, stockList[ws.cell(i, 28).value]))
     
     if ws.cell(row=i, column=6).value == "판매중":
       if int(ws.cell(row=i, column=13).value) != 0:
         if prdDetailInfoProduct in excProducts:
-          excProductsCheckList.append("○ {} / 상태 : {} / 재고수량 : {}".format(ws.cell(i, 28).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
+          excProductsCheckList.append("○ {} / 상품번호 : {} / 상태 : {} / 재고수량 : {}".format(ws.cell(i, 28).value, ws.cell(i, 28).value, ws.cell(i, 6).value, ws.cell(i, 13).value))
           
   except Exception as e:
     matchingErrList.append('row : {} / {}'.format(i, e))
