4a79be12bcc7b480a3351645a622fb7af337e005
shong <shongtruth@gmail.com>
2025-03-14 15:38:48 +0900

    feat: 데이터 추가용 코드

--- pythonDB_SQLite/제품정보_DB생성(데이터추가).py
+++ pythonDB_SQLite/제품정보_DB생성(데이터추가).py
@@ -1,8 +1,6 @@
 import shutil
 import sqlite3
 import productsData
-import productsCode
-import productsPrice
 
 # SQLite 연결 객체 생성
 con = sqlite3.connect('D:/1.업무/10.기타자료/Development/db/productsData.db')
@@ -15,44 +13,20 @@ from_dbFile_path = 'D:/1.업무/10.기타자료/Development/db/productsData.db'
 to_dbFile_path = 'D:/1.업무/10.기타자료/Development/db/backup/productsData_backup.db'
 shutil.copy(from_dbFile_path, to_dbFile_path)
 
-prdInfo = productsData.product_list
-codeInfo = productsCode.productCode
-salePriceInfo = productsPrice.productsSalePrice
-dealPriceInfo = productsPrice.productsDealPrice
+excPrdInfo = productsData.excPrd_List
 
-# colorInfo = productsData.color_list
-# sizeInfo = productsData.size_list
-# capInfo = productsData.capList
-
-for i in dealPriceInfo:
+# 데이터 추가 기준 초기행 지정
+initRow = 4
+for i in excPrdInfo:
   try:
-    query = "UPDATE ProductsData SET dealPrice=? WHERE prdName = ?"
-    cur.execute(query, (dealPriceInfo[i], i))
+    query = "UPDATE ProductsData SET ExcProducts=? WHERE rowid = ?"
+    cur.execute(query, (i, initRow))
+    initRow += 1
   except Exception as e:
     print('{} / {}'.format(i, e))
 
-# for idx, color in enumerate(colorInfo):
-#   print(idx, color)
-#   query = "UPDATE OR IGNORE ProductsData SET Color=? WHERE rowid = ?"
-#   cur.execute(query, (color, idx + 1))
-
-# for idx, size in enumerate(sizeInfo):
-#   print(idx, size)
-#   query = "UPDATE OR IGNORE ProductsData SET Size=? WHERE rowid = ?"
-#   cur.execute(query, (size, idx + 1))
-
-# for idx, cap in enumerate(capInfo):
-#   print(idx, cap)
-#   query = "UPDATE OR IGNORE ProductsData SET Cap=? WHERE rowid = ?"
-#   cur.execute(query, (cap, idx + 1))
-
 # DB 변경사항 저장
 con.commit()
-
-# cur.execute("SELECT Color from ProductsData WHERE Color IS NOT NULL ORDER BY rowid")
-# data = cur.fetchall()
-# for i in data:
-#   print(i[0])
 
 # DB연결 종료
 con.close()
