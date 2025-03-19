15d785a5f65b545776e7a611fe24db12803a2175
shong <shongtruth@gmail.com>
2025-03-19 17:30:55 +0900

    fix: 사이즈 조절 대상 파일 분류조건 추가

--- img_resize/img_resize(width, height).py
+++ img_resize/img_resize(width, height).py
@@ -6,16 +6,14 @@ from os import makedirs
 
 currPath = os.getcwd()
 files = listdir(currPath)
-files.remove('img_resize.py')
-if exists(currPath + '\\resize'):
-  files.remove('resize')
 
 if not exists(currPath + '\\resize'):
   makedirs(currPath + '\\resize')
 
 for i in files:
-  image1 = Image.open(i)
-  image1 = image1.resize((100, 100))
-  new_image = Image.new('RGB', (image1.width, image1.height), color=(255, 255, 255))
-  new_image.paste(image1, (0, 0))
-  new_image.save('resize\\' + i, 'JPEG')
+  if i.split('.')[-1] == 'jpg':
+    image1 = Image.open(i)
+    image1 = image1.resize((100, 100))
+    new_image = Image.new('RGB', (image1.width, image1.height), color=(255, 255, 255))
+    new_image.paste(image1, (0, 0))
+    new_image.save('resize\\' + i, 'JPEG')
