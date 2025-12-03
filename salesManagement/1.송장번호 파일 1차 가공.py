6eb9385209befb86e0a5f2a4e5cb59b7611b060f
shong <shongtruth@gmail.com>
2025-12-03 14:40:27 +0900

    사은품 체크 조건문 추가

--- salesManagement/1.송장번호 파일 1차 가공.py
+++ salesManagement/1.송장번호 파일 1차 가공.py
@@ -320,6 +320,10 @@ for file in excelFileList:
       for product in product_list:
         if product in str(sheet2.cell(i, 1).value):
           sheet2.cell(i, 2).value = product.replace("(저스틴23)", "").replace("토밍이세트", "토밍이모자세트").replace("해피스노우세트", "해피스노우모자세트")
+          
+          # 사은품 체크
+          if "사은품" in str(sheet2.cell(i, 1).value):
+            sheet2.cell(i, 2).value = "사은품"
           # 스마일비니 단품 판매건 체크
           if "스마일비니" in str(sheet2.cell(i, 1).value) and "(1+1)" not in str(sheet2.cell(i, 1).value):
             orderBeanie.append(str(sheet1.cell(2, 7).value) + " / " + str(sheet2.cell(i, 1).value) + " / " + orderNum)
